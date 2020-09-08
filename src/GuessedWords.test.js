437ad18b05e7b9fbb8e106d9c9cfeef9bf1cfe2f
axelobscura <axosar@gmail.com>
2020-09-08 08:42:16 -0500

    Failing expects

--- src/GuessedWords.test.js
+++ src/GuessedWords.test.js
@@ -20,7 +20,18 @@ test('does not throw warning with default props', () => {
 });
 
 describe('if there are no words guessed', () => {
-
+    let wrapper;
+    beforeEach(() => {
+        wrapper = setup({ guessedWords: [] });
+    });
+    test('renders without error', () => {
+        const component = findByTestAttr(wrapper, 'component-guessed-words');
+        expect(component.length).toBe(1);
+    });
+    test('renders instructions to guessed word', () => {
+        const instructions = findByTestAttr(wrapper, 'guess-instructions');
+        expect(instructions.text().length).not.toBe(0);
+    });
 });
 
 describe('if there are words guessed', () => {
