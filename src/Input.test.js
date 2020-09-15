4a00f70a47ee184f9f275deb25b187cf4c54ee14
axelobscura <axosar@gmail.com>
2020-09-14 19:22:21 -0500

    Adding state to test

--- src/Input.test.js
+++ src/Input.test.js
@@ -1,9 +1,41 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 
-import { findByTestAttr } from '../test/testUtils';
+import { findByTestAttr, storeFactory } from '../test/testUtils';
 import Input from './Input';
 
-test('', () => {
+const setup = (initialState={}) => {
+    const store = storeFactory(initialState);
+    const wrapper = shallow(<input store={store} />);
+    console.log(wrapper.debug());
+};
+
+setup();
+
+describe('render', () => {
+    describe('word has not been guessed', () => {
+        test('renders component without error', () => {
+
+        });
+        test('renders the input box', () => {
+
+        });
+        test('renders the submit button', () => {
+            
+        })
+    });
+    describe('word has been guessed', () => {
+        test('renders component without error', () => {
+
+        });
+        test('does not render the input box', () => {
+
+        });
+        test('does not render the submit button', () => {
+            
+        })
+    });
+});
+describe('update state', () => {
 
 });
