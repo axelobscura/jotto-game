5bb4f2d94a5a327d1a0e9a89392be69eb61ce0f8
axelobscura <axosar@gmail.com>
2020-09-17 08:39:55 -0500

    Correct guess reducer

---
+++ src/integrations.test.js
@@ -0,0 +1,21 @@
+import { storeFactory } from '../test/testUtils';
+import { guessWord } from './actions';
+
+describe('guessWord action dispatcher', () => {
+    describe('no guessed words', () => {
+        test('updates states correctly for unsuccesful guess', () => {
+
+        });
+        test('updates states correctly for succesful guess', () => {
+            
+        })
+    });
+    describe('some guessed words', () => {
+        test('updates states correctly for unsuccesful guess', () => {
+
+        });
+        test('updates states correctly for succesful guess', () => {
+            
+        })
+    });
+});
