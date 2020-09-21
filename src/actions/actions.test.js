749874e6be43c8a6604fdc91fc38262fe65eaddd
axelobscura <axosar@gmail.com>
2020-09-21 12:49:09 -0500

    Installing axios and moxios and tests

---
+++ src/actions/actions.test.js
@@ -0,0 +1,13 @@
+import moxios from 'moxios';
+
+describe('secretWord action creator', () => {
+    beforeEach(() => {
+        moxios.install();
+    });
+    afterEach(() => {
+        moxios.uninstall();
+    });
+    Text('adds response word to state', () => {
+
+    });
+})
