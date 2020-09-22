b7468ef5e68dba7ae2dfad97eb384bb07846b24e
axelobscura <axosar@gmail.com>
2020-09-22 09:02:45 -0500

    Adding matching tests

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,8 +1,10 @@
+import axios from 'axios';
 import { getLetterMatchCount } from '../helpers';
 
 export const actionTypes = {
     CORRECT_GUESS: 'CORRECT_GUESS',
-    GUESS_WORD: 'GUESS_WORD'
+    GUESS_WORD: 'GUESS_WORD',
+    SET_SECRET_WORD: 'SET_SECRET_WORD',
 };
 
 export const guessWord = (guessedWord) => {
@@ -22,4 +24,10 @@ export const guessWord = (guessedWord) => {
             })
         }
     };
+};
+
+export const getSecretWord = () => {
+    return(dispatch) => {
+        axios.get('http://localhost:3000');
+    }
 }
